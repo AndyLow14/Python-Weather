d183b96966ff34bb192df1ad091b1d9cf172be4b
Andy Low <andyjunchenglow@gmail.com>
2022-12-27 21:09:52 +1100

    Widget updates every 60mins

--- PyWeather.py
+++ PyWeather.py
@@ -1,10 +1,14 @@
 # import required modules
 from configparser import ConfigParser
+from datetime import datetime
 import requests
 import tkinter as tk
 
 TUF = 1980
 LOCATION = "North Melbourne, AU"
+MINUTE_INTERVAL = 60
+
+minute_to_ms = MINUTE_INTERVAL * 60000
 
 # extract key from the
 # configuration file
@@ -43,10 +47,28 @@ def getweather(city):
 	else:
 		print("No Content Found")
 
+# Update data every few minutes
+def update():
+
+	now = datetime.now()
+	current_time = now.strftime("%I:%M %p")
+	last_updated_label.config(text=">  " + current_time)
+
+	weather_title.config(text=f'Temp: \nMin: \nMax: ')
+	weather_label.config(text=getweather(LOCATION)[0])
+
+	weather_title2.config(text=f'Sky: \nHumidity: \nWind Spd: ')
+	weather_label2.config(text=getweather(LOCATION)[1])
+
+	weather_title.after(int(minute_to_ms), update)
+
+
 # UI
+last_updated_label = tk.Label(window, font=("Minecraftia", 12), background="black", foreground="white", anchor="w")
+last_updated_label.pack(side="top", anchor="ne", padx=10, pady=(5, 0))
 location_label = tk.Label(window, font=("Minecraftia", 22), background="black", foreground="white", wraplength=590)
-location_label.pack(padx=35, pady=(30, 20))
-location_label.config(text=LOCATION)
+location_label.pack(padx=35, pady=(0, 12))
+location_label.config(text="-  " + LOCATION + "  -")
 
 data_box = tk.Label(window, background="black")
 data_box.pack()
@@ -54,19 +76,19 @@ data_box.pack()
 # Left Column
 weather_title = tk.Label(data_box, font=("Minecraftia", 18), background="black", foreground="white", justify='right')
 weather_title.pack(anchor="w", side=tk.LEFT)
-weather_title.config(text=f'Temp: \nMin: \nMax: ')
 
 weather_label = tk.Label(data_box, font=("Minecraftia", 18), background="black", foreground="white", justify='left')
 weather_label.pack(anchor="w", padx=10, side=tk.LEFT)
-weather_label.config(text=getweather(LOCATION)[0])
+
 
 # Right Column
 weather_title2 = tk.Label(data_box, font=("Minecraftia", 18), background="black", foreground="white", justify='right')
 weather_title2.pack(anchor="w",padx=(30, 0), side=tk.LEFT)
-weather_title2.config(text=f'Sky: \nHumidity: \nWind Spd: ')
 
 weather_label2 = tk.Label(data_box, font=("Minecraftia", 18), background="black", foreground="white", justify='left')
 weather_label2.pack(anchor="w", padx=(10, 0), side=tk.LEFT)
-weather_label2.config(text=getweather(LOCATION)[1])
+
+
+update()
 
 tk.mainloop()
